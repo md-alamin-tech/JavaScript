84ee25a549f4c7ae55570e5773b895d3cbc88191
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-11-18 00:45:42 +0600

    Larne about DOM

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+let heading = document.getElementById("myId");
+console.dir(heading);
+
+let headings = document.getElementsByClassName("myClass");
+console.dir(headings);
+console.log(headings);
+
+let firstEl = document.querySelector("p");
+console.dir(firstEl);
+
+let allEl = document.querySelectorAll("p");
+console.dir(allEl);
+
