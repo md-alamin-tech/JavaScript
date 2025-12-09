6aba2bdce9df32d8beb0e99ec9274507793e3737
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-12-10 03:09:03 +0600

    Reduce

---
+++ ES6/reduce.js
@@ -0,0 +1,13 @@
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// let sum = arr.reduce((prevValue, currentValue) => {
+//     return prevValue + currentValue;
+// }, 0);
+
+// console.log(sum);
+
+let name = ["a", "l", "a", "m", "i", "n"];
+
+let count = name.reduce((prevValue, NextValue) => {
+    
+})
