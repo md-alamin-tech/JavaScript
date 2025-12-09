648cf39e891565a06504eb9cb3456fddcd59bf9d
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-12-09 16:50:31 +0600

    let and const

---
+++ ES6/letConst.js
@@ -0,0 +1,30 @@
+let count = 0;
+function increment() {
+  count++;
+}
+
+increment();
+console.log(count);
+
+let incrementArrow = () => {
+  count++;
+};
+
+incrementArrow();
+
+console.log(incrementArrow);
+{
+  let count = 0;
+  function increment() {
+    count++;
+  }
+}
+
+const obj = {
+  name: "al",
+  age: 25,
+};
+obj.name = "al amin";
+
+
+console.log(obj.name);
