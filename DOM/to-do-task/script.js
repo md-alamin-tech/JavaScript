ab02a1c09c2cd666671f16369cc61803ba1965cd
md-alamin-tech <contact.alamin.tech@gmail.com>
2025-12-10 18:11:52 +0600

    Complete Incomplete to do task

--- DOM/to-do-task/script.js
+++ DOM/to-do-task/script.js
@@ -1,27 +1,46 @@
 // Incomple to-do
 
-function genarateIncomplteToDo(taskName) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("item");
+function generateIncomplteToDo(taskName) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
 
-    const inputItem = document.createElement("input");
-    inputItem.setAttribute("type", "checkBox");
+  const inputItem = document.createElement("input");
+  inputItem.setAttribute("type", "checkbox");
 
-    const lebel = document.createElement("lebe");
-    lebel.innerText = taskName;
+  const label = document.createElement("label");
+  label.innerText = taskName;
 
-    return listItem.append(inputItem, lebel);
+  listItem.append(inputItem, label);
+
+  return listItem;
 }
 
 // Complete to-do
 
-function genarateComplteToDo(taskName) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("item");
+function generateComplteToDo(taskName) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
 
-    const button = document.createElement("button");
-    button.classList.add("button");
+  const button = document.createElement("button");
+  button.classList.add("button");
 
-    return listItem.append(taskName, " ", button);
+  listItem.append(taskName, " ", button);
 
+  return listItem;
 }
+
+const toDoForm = document.querySelector("form");
+
+toDoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const taskInput = document.querySelector("#new-task");
+  const taskName = taskInput.value;
+
+  const listItomToAdd = generateIncomplteToDo(taskName);
+  const listContainer = document.querySelector("#items");
+
+  listContainer.appendChild(listItomToAdd);
+
+  taskInput.value = "";
+});
